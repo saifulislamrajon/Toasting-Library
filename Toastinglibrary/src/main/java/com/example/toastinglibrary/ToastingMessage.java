bb0d726d569ee0f5162c151fce7ee572476e1984
saifulrajon <saifulrajon87@gmail.com>
2021-03-11 14:00:47 +0600

    add new method

--- Toastinglibrary/src/main/java/com/example/toastinglibrary/ToastingMessage.java
+++ Toastinglibrary/src/main/java/com/example/toastinglibrary/ToastingMessage.java
@@ -10,4 +10,15 @@ public class ToastingMessage {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
 
     }
+
+    public static void toastMessageShortOrLong(Context c, boolean isLong, String message) {
+
+        if (isLong) {
+            Toast.makeText(c, message, Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
 }
